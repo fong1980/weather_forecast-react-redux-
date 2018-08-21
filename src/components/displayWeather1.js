65d0919871e76d48e7c73b590c3e844e7cbbc258
fong1980 <fong1980@gmail.com>
2018-08-21 10:14:28 +0200

    chart done for now, back to css

--- src/components/displayWeather1.js
+++ src/components/displayWeather1.js
@@ -18,39 +18,21 @@ class DisplayWeather1 extends PureComponent {
     let temp = [];
     let time = [];
 
-    // let data = {
-    //   labels: ["January", "February", "March", "April", "May", "June", "July"],
-    //   datasets: [
-    //     {
-    //       label: "My First dataset",
-    //       fill: false,
-    //       lineTension: 0.1,
-    //       backgroundColor: "rgba(75,192,192,0.4)",
-    //       borderColor: "rgba(75,192,192,1)",
-    //       borderCapStyle: "butt",
-    //       borderDash: [],
-    //       borderDashOffset: 0.0,
-    //       borderJoinStyle: "miter",
-    //       pointBorderColor: "rgba(75,192,192,1)",
-    //       pointBackgroundColor: "#fff",
-    //       pointBorderWidth: 1,
-    //       pointHoverRadius: 5,
-    //       pointHoverBackgroundColor: "rgba(75,192,192,1)",
-    //       pointHoverBorderColor: "rgba(220,220,220,1)",
-    //       pointHoverBorderWidth: 2,
-    //       pointRadius: 1,
-    //       pointHitRadius: 10,
-    //       data: [65, 59, 100, 81, 56, 55, 40]
-    //     }
-    //   ]
-    // };
-
     return (
       <div>
         {ListBy5days.map((day, i) => {
           //-----
 
           let data = {
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    beginAtZero: true
+                  }
+                }
+              ]
+            },
             labels: [
               "January",
               "February",
@@ -62,39 +44,24 @@ class DisplayWeather1 extends PureComponent {
             ],
             datasets: [
               {
-                label: "My First dataset",
+                label: "Temp",
                 fill: false,
-                lineTension: 0.1,
-                backgroundColor: "rgba(75,192,192,0.4)",
                 borderColor: "rgba(75,192,192,1)",
-                borderCapStyle: "butt",
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: "miter",
-                pointBorderColor: "rgba(75,192,192,1)",
                 pointBackgroundColor: "#fff",
-                pointBorderWidth: 1,
-                pointHoverRadius: 5,
-                pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                pointHoverBorderColor: "rgba(220,220,220,1)",
-                pointHoverBorderWidth: 2,
-                pointRadius: 1,
-                pointHitRadius: 10,
+                pointRadius: 5,
                 data: [65, 59, 100, 81, 56, 55, 40]
               }
             ]
           };
 
-          //-------
-
-          // data.datasets[0].data = []; //reset temperature
-          // data.labels = []; //reset time
-
+          data.datasets[0].data = [];
+          data.labels = [];
+          data.datasets[0].label = day[0].dt_txt.substr(0, 10); //label
           data.labels = day.map((x, i) => x.dt_txt.substr(10, 13));
-
           data.datasets[0].data = day.map((x, i) =>
             KelvinToCelsius(x.main.temp)
           );
+
           console.log(temp, "---");
           // console.log(data.datasets[0].data, "data");
           // console.log(data.datasets[0].data, "temp");
@@ -126,3 +93,16 @@ export default connect(
 //["January", "February", "March", "April", "May", "June", "July"]
 
 // temp [14, 14, 16, 22, 24, 25, 24, 22]
+
+// options: {
+//   scales: {
+//       yAxes: [{
+//           ticks: {
+//               beginAtZero:true
+//           }
+//       }]
+//   },
+//   tooltips: {
+//     mode: "label"
+//   }
+// }
